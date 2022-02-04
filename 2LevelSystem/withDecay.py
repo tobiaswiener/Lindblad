f2391103bb676a713bdfc5164c571813f6a85929
tobiaswiener <tobias.wiener@physik.uni-augsburg.de>
2022-02-04 15:19:05 +0100

    expand functions implemented<

--- 2LevelSystem/withDecay.py
+++ 2LevelSystem/withDecay.py
@@ -28,11 +28,31 @@ def transform_vektor(vec, V_inv):
     vec_new = np.einsum("ij,j->i", V_inv, vec)
     return vec_new
 
+def right_eigenvalue_equation(L, right, eigvals):
+    lhs = np.einsum("jk,ki->ji",L,right)
+    rhs = np.einsum("i,ji->ji",eigvals,right)
+    return np.allclose(lhs,rhs)
+def left_eigenvalue_equation(L,left,eigvals):
+    lhs = np.einsum("jk,ki->ji",L.conj().T,left)
+    rhs = np.einsum("i,ji->ji",eigvals.conj(),left)
+    return np.allclose(lhs,rhs)
 
+def expand_rho(left,right,vector):
+    left_vector = np.einsum("ki,k->i",left.conj(),vector)
+    expanded = np.einsum("ki,i->k",right,left_vector)
+    return expanded
+
+def expand_rho_t(eigvals,left,right,rho_0,t):
+    exp_eigvals = np.exp(np.einsum("i,t->it", eigvals, t))
+
+    left_vector = np.einsum("ki,k->i",left.conj(),rho_0)
+    rho_t = np.einsum("it,ki,i->kt",exp_eigvals,right,left_vector)
+
+    return rho_t
 if __name__ == '__main__':
     energy = 1
-    omega = 2
-    gamma = 0.4
+    omega = 0.5
+    gamma = 0.2
     rho_00 = 0.
     rho_01 = 0.
     rho_10 = 0.
@@ -41,22 +61,27 @@ if __name__ == '__main__':
     t = np.linspace(0, 5, 100)
 
     L = make_liouvillian(energy, omega, gamma)
-    eigval, V = np.linalg.eig(L)
-    V_inv = np.linalg.inv(V)
-    L_tilde = transform_matrix(L, V, V_inv)
+    eigvals, left, right = sp.linalg.eig(L, left=True, right=True)
+
     rho_0 = np.array([rho_00, rho_01, rho_10, rho_11])
 
-    U_tilde = np.zeros((DIM_LIOUVILLE_SPACE, DIM_LIOUVILLE_SPACE, t.size), dtype=np.complex64)
+    if False:
+        V_inv = np.linalg.inv(V)
+        L_tilde = transform_matrix(L, V, V_inv)
 
-    for i in range(DIM_LIOUVILLE_SPACE):
-        U_tilde[i, i, :] = np.exp(eigval[i] * t)
+        U_tilde = np.zeros((DIM_LIOUVILLE_SPACE, DIM_LIOUVILLE_SPACE, t.size), dtype=np.complex64)
 
-    U = np.einsum("ik,klt,lj->ijt", V, U_tilde, V_inv)
-    rho_t = np.einsum("ijt,j->it", U, rho_0)
+        for i in range(DIM_LIOUVILLE_SPACE):
+            U_tilde[i, i, :] = np.exp(eigvals[i] * t)
 
+        U = np.einsum("ik,klt,lj->ijt", V, U_tilde, V_inv)
+        rho_t = np.einsum("ijt,j->it", U, rho_0)
+
+
+    rho_t = expand_rho_t(eigvals,left,right,rho_0,t)
     ## check: diagonal elements of density matrix sum up to one
-    assert np.allclose(rho_t[0, :] + rho_t[3, :], np.ones_like(rho_t[0, :]))
-    assert np.allclose(rho_t[1, :] - rho_t[2, :].T.conj(), np.zeros_like(rho_t[1, :]))
+    #assert np.allclose(rho_t[0, :] + rho_t[3, :], np.ones_like(rho_t[0, :]))
+    #assert np.allclose(rho_t[1, :] - rho_t[2, :].T.conj(), np.zeros_like(rho_t[1, :]))
 
     # plot populations and coherences
     fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -70,3 +95,4 @@ if __name__ == '__main__':
     ax2.plot(t, np.angle(rho_t[1]),  label=r"$arg(\rho_{01})$")
     ax2.set(xlabel="time", ylabel="coherence")
     ax2.legend()
+    plt.show()
