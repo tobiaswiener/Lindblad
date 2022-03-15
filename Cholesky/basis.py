61754aae2ec0edd1a0cb94fcc4208ef7d5bcd122
tobiaswiener <tobias.wiener@physik.uni-augsburg.de>
2022-03-15 20:36:53 +0100

    basis and state expansion

---
+++ Cholesky/basis.py
@@ -0,0 +1,61 @@
+import itertools
+
+import numpy as np
+
+
+
+class PauliBasis():
+    pauli_idxs = (0, 1, 2, 3)
+    s0 = np.matrix([[1, 0], [0, 1]])
+    s1 = np.matrix([[0, 1], [1, 0]])
+    s2 = np.matrix([[0, -1j], [1j, 0]])
+    s3 = np.matrix([[1, 0], [0, -1]])
+    s = np.array((s0, s1, s2, s3))
+
+    def __init__(self, n):
+
+
+
+        self.n = n
+        self.dim_hilbert = 2**n
+        self.dim_liouville = 4**n
+
+        self.basis_states = {}
+        self._init_basis_states()
+
+
+
+    def _init_basis_states(self):
+        for j in itertools.product(self.pauli_idxs, repeat=self.n):
+            self.basis_states[j] = self._make_basis_element(j)
+
+    def _make_basis_element(self, j):
+        sigma = 1.
+        for k in range(self.n):
+            sigma = np.kron(sigma, self.s[j[k]])
+        return sigma
+
+    def _get_coefficient(self, j, M):
+        cj = np.einsum("ij,ji", self.basis_states[j], M)
+        return cj
+
+    def matrix_to_coefficients(self, M):
+        c = {}
+        for j, sigma in self.basis_states.items():
+            cj = self._get_coefficient(j, M)
+            c[j] = cj
+
+        return c
+
+    def coefficients_to_matrix(self, c):
+        M = np.zeros((self.dim_hilbert, self.dim_hilbert), dtype=complex)
+        for j, c_j in c.items():
+            M += c_j * self.basis_states[j]
+
+        M = M / self.dim_hilbert
+        return M
+
+
+
+if __name__ == '__main__':
+    basis = PauliBasis(6)
---
+++ Cholesky/state.py
@@ -0,0 +1,50 @@
+from basis import PauliBasis
+import numpy as np
+import math
+from collections import defaultdict
+SEED = 1234
+
+
+class State():
+    rng = np.random.default_rng(SEED)
+
+    def __init__(self, n=1, c=None,):
+        if c == None:
+            c = {}
+        self.basis = PauliBasis(n)
+        assert State._c_is_valid(n,c), "coefficient dict is not valid."
+        self.c = c
+
+
+    @classmethod
+    def _c_is_valid(cls, n, c):
+        basis = PauliBasis(n)
+        valid = True
+        for j in c.keys():
+            if j not in basis.basis_states:
+                valid = False
+        return valid
+
+    @classmethod
+    def from_matrix(cls, matrix):
+        assert matrix.shape[0] == matrix.shape[1], "matrix must be square"
+        assert math.log(matrix.shape[0], 2).is_integer(), "matrix dimension must be power of 2!"
+        n = int(math.log(matrix.shape[0], 2))
+        basis = PauliBasis(n)
+        c = basis.matrix_to_coefficients(matrix)
+        return cls(n, c)
+
+    @classmethod
+    def from_random(cls, n):
+        basis = PauliBasis(n,)
+        c = {}
+        for j in basis.basis_states.keys():
+            c[j] = cls.rng.random()
+        return cls(n, c)
+
+
+    def get_matrix(self):
+        return self.basis.coefficients_to_matrix(self.c)
+
+if __name__ == '__main__':
+    pass
